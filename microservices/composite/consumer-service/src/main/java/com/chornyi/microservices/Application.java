d2abbbe1d6245bd33736ef14878f1da7c03fd930
Taras Chornyi <taras.chornyi@she-systems.de>
2018-07-22 18:50:35 +0300

    Added skeleton of the consumer service.

---
+++ microservices/composite/consumer-service/src/main/java/com/chornyi/microservices/Application.java
@@ -0,0 +1,21 @@
+package com.chornyi.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        new SpringApplication(Application.class).run(args);
+    }
+}
